3d63cfdb85d2dfc1d8037a9254b582e4343e670a
Jamie <peebles.jamie1@gmail.com>
2019-02-05 13:41:34 -0600

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -49,8 +49,26 @@ UserSchema.methods.generateAuthToken = function() {
     // "CONCAT" method --> works like a PUSH function
     user.tokens = user.tokens.concat([{ access, token }]);
 
-    user.save().then(() => {
+    return user.save().then(() => {
         return token;
+    });
+};
+
+// MODEL - METHOD
+UserSchema.statics.findByToken = function(token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
     });
 };
 
